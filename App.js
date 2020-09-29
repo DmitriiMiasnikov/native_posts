8c8b4e0fd72bc2662a5ab5bf7ab43bd46c37aed4
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2020-09-29 16:37:08 +0300

    add screens

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { AppLoading } from 'expo';
+import { bootstrap } from './src/bootstrap';
+import { createStackNavigator } from '@react-navigation/stack';
+import { MainScreen } from './src/screens/MainScreen';
+import { PostScreen } from './src/screens/PostScreen';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [isReady, setIsReady] = useState(false)
+  if (isReady) {
+    return <AppLoading startAsync={bootstrap} onFinish={() => setIsReady(true)} onError={(err) => console.log(err)} />
+  }
+  return <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name='Main' component={MainScreen} />
+      <Stack.Screen name='Post' component={PostScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
