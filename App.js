a8185883531c90ccb1bf41820360455a07b119c1
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2020-09-30 13:36:10 +0300

    add icon title

--- App.js
+++ App.js
@@ -1,23 +1,37 @@
 import React, { useState } from 'react';
+import { Text, StyleSheet, Platform } from 'react-native'
 import { AppLoading } from 'expo';
 import { bootstrap } from './src/bootstrap';
 import { createStackNavigator } from '@react-navigation/stack';
 import { MainScreen } from './src/screens/MainScreen';
 import { PostScreen } from './src/screens/PostScreen';
 import { NavigationContainer } from '@react-navigation/native';
-import {THEME} from './src/theme';
+import { THEME } from './src/theme';
+import { AppHeaderIcon } from './src/components/AppHeaderIcon';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 
 const Stack = createStackNavigator();
 
 export default function App() {
   const stylesMainPage = {
-    headerTitle: 'Main page',
+    headerTitle: (
+      <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+        <Item title={'Take photo'} iconName={'ios-camera'} onPress={() => console.log('press photo')} />
+        <Text style={styles.title}>Main page</Text>
+      </HeaderButtons>
+    ),
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+        <Item title={'Take photo'} iconName={'ios-camera'} onPress={() => console.log('press photo')} />
+      </HeaderButtons>
+    ),
+    // headerTitle: 'Main page',
     headerStyle: {
       backgroundColor: THEME.MAIN_COLOR
     },
     headerTintColor: '#fff'
   }
-  const stylesPostPage = ({route}) => ({
+  const stylesPostPage = ({ route }) => ({
     headerTitle: `Пост от ${new Date(route.params.date).toLocaleDateString()}`,
     headerStyle: {
       backgroundColor: THEME.MAIN_COLOR
@@ -30,8 +44,15 @@ export default function App() {
   }
   return <NavigationContainer>
     <Stack.Navigator>
-      <Stack.Screen name='Main' component={MainScreen} options={stylesMainPage}/>
-      <Stack.Screen name='Post' component={PostScreen} options={stylesPostPage}/>
+      <Stack.Screen name='Main' component={MainScreen} options={stylesMainPage} />
+      <Stack.Screen name='Post' component={PostScreen} options={stylesPostPage} />
     </Stack.Navigator>
   </NavigationContainer>
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 22,
+    color: Platform.OS === 'android' ? 'white' : THEME.MAIN_COLOR
+  }
+})
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { View, StyleSheet, FlatList } from 'react-native';
 import { DATA } from '../data';
-import { Post } from './../../src/components/Post'
+import { Post } from './../../src/components/Post';
 
 export const MainScreen = ({ navigation }) => {
     const openPostHandler = (post) => {
-        navigation.navigate('Post', {postId: post.id, date: post.date})
+        navigation.navigate('Post', { postId: post.id, date: post.date })
     }
     return (
         <View style={styles.wrapper}>
-            <FlatList data={DATA} keyExtractor={post => post.id.toString()} 
+            <FlatList data={DATA} keyExtractor={post => post.id.toString()}
                 renderItem={({ item }) => <Post post={item} onOpen={openPostHandler} />} />
         </View>
     )
