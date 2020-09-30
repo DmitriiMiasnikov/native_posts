7cece71f5e32feb1e695792f234de4a0f82c1341
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2020-09-30 12:58:43 +0300

    add compoent post

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,21 +1,47 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, Button, Dimensions, ScrollView, Alert } from 'react-native';
+import { DATA } from '../data';
+import { THEME } from '../theme';
 
 export const PostScreen = ({ route }) => {
+    const removeHandler = () => {
+        Alert.alert(
+            "Удаление поста",
+            "Вы точно хотите удалить пост?",
+            [
+                {
+                    text: "Отменить",
+                    style: "cancel"
+                },
+                { text: "Удалить", style: 'destructive', onPress: () => console.log("OK Pressed") }
+            ],
+            { cancelable: false }
+        );
+    }
     const postId = route.params?.postId
+    const post = DATA.find(el => el.id === postId)
     return (
-        <View style={styles.center}>
-            <Text>
-                {postId}
-            </Text>
-        </View>
+        <ScrollView style={styles.center}>
+            <Image source={{ uri: post.img }} style={styles.image} />
+            <View style={styles.textWrap}>
+                <Text style={styles.title}>
+                    {post.text}
+                </Text>
+            </View>
+            <Button title={'Удалить'} color={THEME.DANGER_COLOR} onPress={removeHandler} />
+        </ScrollView>
     )
 }
-
+const screen = Dimensions.get('screen');
 const styles = StyleSheet.create({
-    center: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+    image: {
+        width: screen.width,
+        height: 200
+    },
+    textWrap: {
+        padding: 10
+    },
+    title: {
+        fontSize: 24
     }
 })
