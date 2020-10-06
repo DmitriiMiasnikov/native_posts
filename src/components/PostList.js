5da63eccb39db7382cb9cd568d7c8f61aaa7a14a
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2020-10-07 01:31:28 +0300

    remove data file

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import { View, StyleSheet, FlatList } from 'react-native';
+import { View, StyleSheet, FlatList, Text } from 'react-native';
 import {Post} from './Post'
 
 export const PostList = ({ data, onOpen }) => {
+    if (!data.length) {
+        return <View style={styles.wrapper}>
+            <Text style={styles.noItems}>
+                Постов нет
+            </Text>
+        </View>
+    }
+
     return (
         <View style={styles.wrapper}>
             <FlatList data={data} keyExtractor={post => post.id.toString()}
@@ -18,5 +26,10 @@ const styles = StyleSheet.create({
     },
     wrapper: {
         padding: 15
+    },
+    noItems: {
+        fontSize: 24,
+        textAlign: 'center',
+        marginVertical: 10
     }
 })
--- src/store/actions/postActions.js
+++ src/store/actions/postActions.js
@@ -1,8 +1,7 @@
-import { DATA } from "../../data";
 import { ADD_POST, LOAD_POSTS, REMOVE_POST, TOGGLE_BOOKED } from './../types';
 
 export const loadPosts = () => {
-    return { type: LOAD_POSTS, payload: DATA }
+    return { type: LOAD_POSTS, payload: [] }
 }
 
 export const toggleBooked = (id) => {
